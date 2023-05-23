6206d8ea7f06052ab3d38371a2f9ffc2c0baf153
Жахангир Таиров <jahangirtairov123@gmail.com>
2023-05-23 18:56:33 +0600

    removed extension

--- DeveloperTool/Extensions/UIViewController+Extensions.swift
+++ DeveloperTool/Extensions/UIViewController+Extensions.swift
@@ -5,14 +5,14 @@
 //  Created by &&TairoV on 22.05.2023.
 //
 
-import UIKit
-
-extension UIViewController {
-    
-    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
-        if case .motionShake = motion {
-            DeveloperToolManager.shared.menuController.modalPresentationStyle = .fullScreen
-            present(DeveloperToolManager.shared.menuController.embedInNavigation(), animated: true)
-        }
-    }
-}
+//import UIKit
+//
+//extension UIViewController {
+//    
+//    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+//        if case .motionShake = motion {
+//            DeveloperToolManager.shared.menuController.modalPresentationStyle = .fullScreen
+//            present(DeveloperToolManager.shared.menuController.embedInNavigation(), animated: true)
+//        }
+//    }
+//}
